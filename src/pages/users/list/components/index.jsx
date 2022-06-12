c5a06e754c92b3abcbab6de42fb8a74e865e3b7f
arfan21 <arfan.jadul10@gmail.com>
2022-06-12 19:49:59 +0700

    remove action in manage users

--- src/pages/users/list/components/index.jsx
+++ src/pages/users/list/components/index.jsx
@@ -1,13 +1,10 @@
 import CustomTable, {
     DateRangeColumnFilter,
 } from 'components/CustomTable';
-import Modal from 'components/Modal';
-import ModalAction from 'components/ModalAction';
-import users from 'constant/api/users';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 const ListUsersContent = (props) => {
-    const { data: dataUsers, setEventDelete } = props;
+    const { data: dataUsers } = props;
 
     const columns = useMemo(
         () => [
@@ -36,66 +33,20 @@ const ListUsersContent = (props) => {
                 filter: 'dateBetween',
                 Filter: DateRangeColumnFilter,
             },
-            {
-                Header: 'Action',
-                accessor: 'action',
-                filterable: false,
-            },
         ],
         [],
     );
-
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
-
-    const deleteSelectedUser = async () => {
-        const toastId = 'deleteusers';
-        try {
-            window.showLoader(true);
-            await users.delete(selectedUser?.id);
-            window.showLoader(false);
-            window.showToast(
-                toastId,
-                'info',
-                `success delete users "${selectedUser?.name}"`,
-            );
-            setEventDelete(true);
-        } catch (error) {
-            window.showLoader(false);
-            window.showToast(
-                toastId,
-                'error',
-                error?.response?.data?.message ?? error?.message,
-            );
-        }
-    };
 
     const data = useMemo(
         () =>
             dataUsers.map((item) => ({
                 ...item,
-                action: (
-                    <ModalAction
-                        urlEdit={`/users/edit/${item?.id}`}
-                        item={item}
-                        setSelectedUser={setSelectedUser}
-                        setModalDeleteOpen={setIsModalDeleteOpen}
-                    />
-                ),
             })),
         [dataUsers],
     );
 
     return (
         <>
-            <Modal
-                title="Are you sure?"
-                label={`Do you really want to delete user "${selectedUser?.name}" ? This process cannot be undone`}
-                btnTitle="Delete"
-                isOpen={isModalDeleteOpen}
-                setIsOpen={setIsModalDeleteOpen}
-                action={deleteSelectedUser}
-            />
             <CustomTable data={data} columns={columns} />
         </>
     );
--- src/pages/users/list/index.jsx
+++ src/pages/users/list/index.jsx
@@ -6,34 +6,29 @@ import ListUsersContent from './components';
 
 const ListUsers = () => {
     const [data, setData] = useState([]);
-    const [eventDelete, setEventDelete] = useState(true);
 
     useEffect(async () => {
         const toastId = 'fetchusers';
-        if (eventDelete) {
-            try {
-                window.showLoader(true);
-                const res = await users.getAll();
-                const dataMapped = res.data.map((item) => ({
-                    ...item,
-                    id: item?.id,
-                    role: item?.role ?? '',
-                    created_at: new Date(
-                        item.created_at,
-                    ).toLocaleString(),
-                }));
-                setData(dataMapped);
-                window.showLoader(false);
-                setEventDelete(false);
-            } catch (error) {
-                window.showLoader(false);
-                window.showToast(
-                    toastId,
-                    'error',
-                    error?.response?.data?.message ?? error?.message,
-                );
-                setEventDelete(false);
-            }
+        try {
+            window.showLoader(true);
+            const res = await users.getAll();
+            const dataMapped = res.data.map((item) => ({
+                ...item,
+                id: item?.id,
+                role: item?.role ?? '',
+                created_at: new Date(
+                    item.created_at,
+                ).toLocaleString(),
+            }));
+            setData(dataMapped);
+            window.showLoader(false);
+        } catch (error) {
+            window.showLoader(false);
+            window.showToast(
+                toastId,
+                'error',
+                error?.response?.data?.message ?? error?.message,
+            );
         }
     }, []);
 
@@ -42,10 +37,7 @@ const ListUsers = () => {
             mainTitle="Manage Users"
             childTitle="Manage users in your system"
         >
-            <ListUsersContent
-                data={data}
-                setEventDelete={setEventDelete}
-            />
+            <ListUsersContent data={data} />
         </LayoutBack>
     );
 };
